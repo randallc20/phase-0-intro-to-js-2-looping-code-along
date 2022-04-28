6843f35202ecb4e6d92a95b96a45dd699af97e69
chrisrandall <chris.f.randall@gmail.com>
2022-04-27 19:50:46 -0500

    commit tree a261f65ad2b9f20b96bee68e7f8170be1ef80153

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const signedCards = [];
+
+function writeCards(names, eventName){
+    for(let i = 0; i < names.length; i++){
+        signedCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return signedCards;
+
+
+}
+
+
+function countDown(posInt){
+    if(posInt < 0){
+        return "you must input a positive number.";
+    }
+    else{
+        while(posInt>=0){
+            console.log(posInt);
+            posInt--;
+        }
+    }
+}
